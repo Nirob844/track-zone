e125a1eb3ee7d1bc38d34d6cebf462778cf916ad
Nirob Hasan <nirob427417@gmail.com>
2024-03-04 17:18:07 +0600

    local time to other time convert

---
+++ src/components/clock-list/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClockList = () => {
+    return (
+        <div>
+            <p>ClockLisrt</p>
+        </div>
+    );
+};
+
+export default ClockList;
---
+++ src/components/local-clock/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LocalClock = () => {
+    return (
+        <div>LocalClock</div>
+    )
+}
+
+export default LocalClock
