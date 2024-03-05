7c6e3251e72c5dc8dbb7e96623559585d83ecc19
Nirob Hasan <nirob427417@gmail.com>
2024-03-05 15:39:26 +0600

    Display and Edit Local Time

--- src/components/local-clock/index.jsx
+++ src/components/local-clock/index.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import useClock from '../../hooks/useClock';
+import ClockActions from '../shared/clock-actions';
+import ClockDisplay from '../shared/clock-display';
 
-export const LocalClock = () => {
+export const LocalClock = ({ clock, updateClock }) => {
+
+    const { date, timezone, offset } = useClock(
+        clock.timezone,
+        clock.offset
+    )
+
+    useEffect(() => {
+        updateClock({
+            date,
+            timezone,
+            offset
+        })
+    }, [date])
     return (
-        <div>LocalClock</div>
+        <div>
+            {date && (
+                <ClockDisplay
+                    date={date}
+                    title={clock.title}
+                    timezone={timezone}
+                    offset={offset} />
+            )}
+            <ClockActions local={true} clock={clock} updateClock={updateClock} />
+        </div>
     )
 }
 
---
+++ src/components/shared/clock-display/index.jsx
@@ -0,0 +1,20 @@
+import { format } from 'date-fns';
+import React from 'react';
+
+const ClockDisplay = ({ date, title, timezone, offset }) => {
+
+    console.log(format(date, 'yyyy-MM-dd hh:mm:ss aaa'));
+    let offsetHr = offset / 60
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h3>{format(date, 'yyyy-MM-dd hh:mm:ss aaa')}</h3>
+            <p>{timezone}
+                {offset > 0
+                    ? ` +${Math.abs(offsetHr)}`
+                    : ` -${Math.abs(offsetHr)}`}</p>
+        </div>
+    )
+}
+
+export default ClockDisplay
